f0f16c73ea20563a9ea37708aa83a7114744b45c
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-09-21 17:43:41 -0500

    agregar stack

--- src/components/ListFlag.js
+++ src/components/ListFlag.js
@@ -4,43 +4,59 @@ import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 import Entypo from 'react-native-vector-icons/Entypo';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Animated from 'react-native-reanimated';
+import { useNavigation } from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const ListFlag = ({ data, opacity, scale }) => {
+    const navigation = useNavigation();
     console.log('log: ', data)
     const { title } = data;
+
+    const handlePress = () => {
+        navigation.navigate('Detail')
+    }
+
     return (
-        <Animated.View style={{
-            marginVertical: 5, display: 'flex',
-            flexDirection: 'row', width: "100%", height: 70,
-            paddingHorizontal: 10,
-            borderBottomWidth: 1,
-            borderBottomColor: "#CBCBCB",
-            shadowRadius: 10,
-            opacity,
-            transform: [{ scale }]
-        }}>
-            <View style={{
-                display: 'flex', flexDirection: 'column', justifyContent: 'space-between',
-                // alignItems: 'center',
-                width: "70%", height: "100%",
+        <TouchableOpacity
+            onPress={handlePress}
+        >
+            <Animated.View style={{
+                marginVertical: 5, display: 'flex',
+                flexDirection: 'row', width: "100%", height: 70,
+                paddingHorizontal: 10,
+                borderBottomWidth: 1,
+                borderBottomColor: "#CBCBCB",
+                shadowRadius: 10,
+                opacity,
+                transform: [{ scale }]
             }}>
-                <Text style={{ fontSize: 17 }}>{title}</Text>
-                <Text style={{ paddingBottom: 10, fontSize: 14, color: "#0078fd" }}>description</Text>
-            </View>
-            <View style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'flex-end',
-                alignItems: 'center',
-                width: "30%",
-                height: "100%",
-            }}>
-                {/* <FontAwesome5 name="info-circle" size={24} color="black" /> */}
-                <Ionicons name="information-circle-outline" size={30} color="#0078fd" />
-                {/* <MaterialIcons name="keyboard-arrow-right" size={24} color="black" /> */}
-                <Entypo name="chevron-right" size={24} color="#cacacb" />
-            </View>
-        </Animated.View>
+
+                <View style={{
+                    display: 'flex', flexDirection: 'column', justifyContent: 'space-between',
+                    // alignItems: 'center',
+                    width: "70%", height: "100%",
+                }}
+
+                >
+                    <Text style={{ fontSize: 17 }}>{title}</Text>
+                    <Text style={{ paddingBottom: 10, fontSize: 14, color: "#0078fd" }}>description</Text>
+                </View>
+                <View style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'flex-end',
+                    alignItems: 'center',
+                    width: "30%",
+                    height: "100%",
+                }}>
+                    {/* <FontAwesome5 name="info-circle" size={24} color="black" /> */}
+                    <Ionicons name="information-circle-outline" size={30} color="#0078fd" />
+                    {/* <MaterialIcons name="keyboard-arrow-right" size={24} color="black" /> */}
+                    <Entypo name="chevron-right" size={24} color="#cacacb" />
+                </View>
+
+            </Animated.View>
+        </TouchableOpacity>
     )
 }
 
--- src/navigator/Drawer.js
+++ src/navigator/Drawer.js
@@ -3,6 +3,7 @@ import { Button, StatusBar, View, Text } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import MapScreen from '../pages/MapScreen';
 import Home from '../pages/Home';
+import Detail from '../pages/Detail';
 
 const Drawer = createDrawerNavigator();
 export default function MyDrawer() {
@@ -17,13 +18,26 @@ export default function MyDrawer() {
                         headerTitle: "Indicadores",
                         headerStyle: {
                             // backgroundColor: '#202023', 
-                            backgroundColor: '#f0e7db',
+                            // backgroundColor: '#f0e7db',
                         },
                         headerTitleStyle: {
                             fontWeight: 'bold',
                         },
                     }}
                     name="Home" component={Home} />
+                <Drawer.Screen
+                    options={{
+                        title: 'Detalle',
+                        headerTitle: "Detalle de lo que presionaste",
+                        headerStyle: {
+                            // backgroundColor: '#202023', 
+                            // backgroundColor: '#f0e7db',
+                        },
+                        headerTitleStyle: {
+                            fontWeight: 'bold',
+                        },
+                    }}
+                    name="Detail" component={Detail} />
                 <Drawer.Screen
                     options={{
                         title: 'Mapa',
