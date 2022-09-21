3b726068a0d4add180bf002bbc08c4f33784acc3
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-09-20 22:21:56 -0500

    add MapView

--- src/pages/MapScreen.js
+++ src/pages/MapScreen.js
@@ -1,10 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+
 
 const MapScreen = () => {
     return (
-        <View>
-            <Text>MapScreen</Text>
+        <View style={{ flex: 1 }}>
+            <MapView
+                provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                style={{ flex: 1 }}
+                region={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.015,
+                    longitudeDelta: 0.0121,
+                }}
+            >
+            </MapView>
         </View>
     )
 }
