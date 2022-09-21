c478dfd70bc49066748ce1ac069f7e4a5b15686d
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-09-21 00:45:07 -0500

    centerPosition

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,26 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import MapView, { Marker } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 import { useLocation } from '../hooks/useLocation';
 import LoadingScreen from '../pages/LoadingScreen';
+import { Fab } from './Fab';
 
 const Map = () => {
 
-    const { hasLocation, initialPosition } = useLocation();
+    const { hasLocation, initialPosition, getCurrentLocation } = useLocation();
+
+    const mapViewRef = useRef();
+    const following = useRef(true);
+
+    const centerPosition = async () => {
+
+        const { latitude, longitude } = await getCurrentLocation();
+
+        // following.current = true;
+
+        mapViewRef.current?.animateCamera({
+            center: { latitude, longitude }
+        });
+    }
 
     if (!hasLocation) {
         return <LoadingScreen />
@@ -14,10 +29,12 @@ const Map = () => {
     return (
         <>
             <MapView
+                ref={(el) => mapViewRef.current = el}
                 style={{ flex: 1 }}
+                showsUserLocation
                 region={{
                     latitude: initialPosition.latitude,
-                    longitude: initialPosition.longitud,
+                    longitude: initialPosition.longitude,
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
                 }}
@@ -31,6 +48,12 @@ const Map = () => {
                     description='Esto es description'
                 />
             </MapView>
+
+            <Fab
+                iconName={'compass-outline'}
+                onPress={centerPosition}
+                style={{ position: 'absolute', bottom: 20, right: 20 }}
+            />
         </>
     )
 }
--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -3,26 +3,39 @@ import React, { useEffect, useState } from 'react';
 
 export const useLocation = () => {
     const [hasLocation, setHasLocation] = useState(false);
-    const [initialPosition, setInitialPosition] = useState('')
+    const [initialPosition, setInitialPosition] = useState({
+        longitude: 0,
+        latitude: 0,
+    })
 
     useEffect(() => {
-        Geolocation.getCurrentPosition(
-            ({ coords }) => {
-                setInitialPosition({
-                    latitude: coords.latitude,
-                    longitud: coords.longitude,
-                })
+        getCurrentLocation()
+            .then(location => {
+                setInitialPosition(location);
                 setHasLocation(true)
-            },
-            (err) => console.log({ err }), { enableHighAccuracy: true }
-        )
+            })
+
     }, [])
 
 
+    const getCurrentLocation = () => {
+        return new Promise((resolve, reject) => {
+            Geolocation.getCurrentPosition(
+                ({ coords }) => {
 
+                    resolve({
+                        latitude: coords.latitude,
+                        longitude: coords.longitude,
+                    })
+                },
+                (err) => reject({ err }), { enableHighAccuracy: true }
+            )
+        })
+    }
 
     return {
         hasLocation,
-        initialPosition
+        initialPosition,
+        getCurrentLocation
     }
 }
