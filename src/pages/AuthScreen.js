66227461c59097a3ca7af5ff26bbe24f73f17210
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-09-21 08:41:40 -0500

    update

--- src/pages/AuthScreen.js
+++ src/pages/AuthScreen.js
@@ -1,19 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { useDispatch } from 'react-redux';
+import { Button } from 'react-native-paper';
 
 const AuthScreen = () => {
     const dispatch = useDispatch();
 
     const signIn = () => {
-
+        const user = "juan perez"
         dispatch(signIn(user));
     }
 
     return (
         <View>
             <Text>AuthScreen</Text>
-
+            <Button title="Empezar" onPress={signIn} />
         </View>
     )
 }
