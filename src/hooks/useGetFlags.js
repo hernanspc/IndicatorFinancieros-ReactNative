f389e6575e1b91cdf2b3bba01dfe204fb29169d4
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-09-21 12:36:02 -0500

    creation customhok

--- src/hooks/useGetFlags.js
+++ src/hooks/useGetFlags.js
@@ -1,9 +1,26 @@
 import React, { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
 
 export const useGetFlags = () => {
+    const [simpleData, setSimpleData] = useState([]);
+    const flag = ["dolar", "euro", "ipc", "uf", "utm"]
 
-    const getFlags = () => {
-        console.log('getFlags ')
+    const getListStaticFlags = async (id) => {
+        return await axios.get(`https://api.cmfchile.cl/api-sbifv3/recursos_api/${id}?apikey=bf6b491ec46caf655a1204c927f559580679f4c4&formato=json`)
+    }
+
+    const getFlags = async () => {
+
+        const flagPromises = flag.map((value) => {
+            return getListStaticFlags(value)
+        })
+
+        let allRealPosts = []
+        const allPosts = await Promise.all(flagPromises)
+        allPosts.map(({ data }) => {
+            allRealPosts.push(...[data])
+        })
+        setSimpleData(allRealPosts)
     }
 
 
@@ -12,6 +29,6 @@ export const useGetFlags = () => {
     }, [])
 
     return {
-
+        simpleData
     }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { useGetFlags } from '../hooks/useGetFlags.js'
+import { FlatList } from 'react-native-gesture-handler';
 
 const Home = () => {
 
-    const { } = useGetFlags();
-
+    const { simpleData } = useGetFlags();
+    console.log('simpleData', simpleData[0])
     return (
         <View>
-            <Text>Home</Text>
+            <FlatList
+                data={simpleData}
+                renderItem={(data => (
+                    <View id={data.index}>
+                        <Text>
+                            {data.index}
+                        </Text>
+                    </View>
+                ))}
+                keyExtractor={item => item.id}
+            />
         </View>
     )
 }
