233a36ede061d56f32a6d9f9969afd09782eb648
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-09-21 20:12:25 -0500

    refactor

--- src/components/ListFlag.js
+++ src/components/ListFlag.js
@@ -1,6 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import { StyleSheet, Text, View } from 'react-native'
 import Entypo from 'react-native-vector-icons/Entypo';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Animated from 'react-native-reanimated';
@@ -10,13 +9,10 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 const ListFlag = ({ data, opacity, scale }) => {
     const navigation = useNavigation();
     const { title, description } = data;
-    console.log('description: ', description)
 
     const handlePress = () => {
         navigation.navigate('Detail', {
-            data: {
-                title: title
-            }
+            data: { title: title }
         })
     }
 
@@ -24,32 +20,12 @@ const ListFlag = ({ data, opacity, scale }) => {
         <TouchableOpacity
             onPress={handlePress}
         >
-            <Animated.View style={{
-                marginVertical: 5, display: 'flex',
-                flexDirection: 'row', width: "100%", height: 70,
-                paddingHorizontal: 10,
-                borderBottomWidth: 1,
-                borderBottomColor: "#CBCBCB",
-                shadowRadius: 10,
-                opacity,
-                transform: [{ scale }]
-            }}>
-
-                <View style={{
-                    display: 'flex', flexDirection: 'column', justifyContent: 'space-between',
-                    width: "70%", height: "100%",
-                }}>
+            <Animated.View style={[styles.container, { opacity, transform: [{ scale }] }]}>
+                <View style={styles.wrapperText}>
                     <Text style={{ fontSize: 17 }}>{title}</Text>
                     <Text style={{ paddingBottom: 10, fontSize: 14, color: "#0078fd" }}>{description}</Text>
                 </View>
-                <View style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'flex-end',
-                    alignItems: 'center',
-                    width: "30%",
-                    height: "100%",
-                }}>
+                <View style={styles.wrapperIcon}>
                     <Ionicons name="information-circle-outline" size={30} color="#0078fd" />
                     <Entypo name="chevron-right" size={24} color="#cacacb" />
                 </View>
@@ -60,4 +36,28 @@ const ListFlag = ({ data, opacity, scale }) => {
 
 export default ListFlag
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        marginVertical: 5, display: 'flex',
+        flexDirection: 'row', width: "100%", height: 70,
+        paddingHorizontal: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: "#CBCBCB",
+        shadowRadius: 10,
+    },
+    wrapperText: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        width: "70%",
+        height: "100%",
+    },
+    wrapperIcon: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        width: "30%",
+        height: "100%",
+    }
+})
